816fbfbf3d6e76cacfbc5f3603b5a688f1b105b4
cdfxddff <49425783+cdfxddff@users.noreply.github.com>
2022-09-21 02:46:11 +0900

    Add files via upload

--- move.js
+++ move.js
@@ -1,13 +1,19 @@
+let i=0;
+
 window.addEventListener("load", async function() {
     const lists = document.getElementById("lis");
+    const tilis = this.document.getElementById("li_f");
     let res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const users = await res.json();
     users.forEach(element => {
         const list = this.document.createElement("li");
-        this.console.log(list);
-        this.console.log(element);
-        list.innerText = element.title;
-        this.console.log(list);//null
-        lists.appendChild(list);//appendhildの処理で詰まる
+        list.setAttribute("id",'l_'+i);
+        const tili = this.document.createElement("li");
+        //読み込んだjsonデータを作ったliタグの中に埋め込む
+        list.innerText = element.body;
+        lists.appendChild(list);
+        tili.innerHTML = `<a href="#l_${i++}">`+element.title+'</a>';
+        tilis.append(tili);
+
     });
 })
